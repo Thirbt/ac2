e2ad0c3a743ce2a571a1607a8ed4204752f3298c
Thirbt <thiago.godo@hotmail.com>
2023-10-10 22:54:16 -0300

    Adicionando entidades do projeto

---
+++ src/main/java/tarefa3/ac2/models/Curso.java
@@ -0,0 +1,31 @@
+package tarefa3.ac2.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 200, nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private Integer cargaHoraria;
+    @ManyToOne
+    private CategoriaCurso categoriaCurso;
+}
