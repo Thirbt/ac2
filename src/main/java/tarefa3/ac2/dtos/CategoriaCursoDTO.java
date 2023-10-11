9586b3fcd229facca11f9b87f551f34da5de0613
Thirbt <thiago.godo@hotmail.com>
2023-10-10 22:53:50 -0300

    Adicionando os DTOS do projeto

---
+++ src/main/java/tarefa3/ac2/dtos/CategoriaCursoDTO.java
@@ -0,0 +1,15 @@
+package tarefa3.ac2.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoriaCursoDTO {
+    private Long id;
+    private String nome;
+}
---
+++ src/main/java/tarefa3/ac2/dtos/CursoDTO.java
@@ -0,0 +1,19 @@
+package tarefa3.ac2.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CursoDTO {
+    private Long id;
+    private String nome;
+    private Integer cargaHoraria;
+    private String nomeCategoriaCurso;
+}
