16731aa73e417547aac7106580c163edc901c72b
Thirbt <thiago.godo@hotmail.com>
2023-10-10 22:54:05 -0300

    Adicionando classe de tratamento de exceptions

---
+++ src/main/java/tarefa3/ac2/exceptions/GeneralException.java
@@ -0,0 +1,7 @@
+package tarefa3.ac2.exceptions;
+
+public class GeneralException extends RuntimeException {
+    public GeneralException(String mensagemError){
+        super(mensagemError);
+    }
+}
