13b8ae3e0a7197b55ad1025252dfedc046308ffd
Thirbt <thiago.godo@hotmail.com>
2023-10-10 23:12:36 -0300

    Criando service para CategoriaCurso

--- src/main/java/tarefa3/ac2/services/CategoriaCursoService.java
+++ src/main/java/tarefa3/ac2/services/CategoriaCursoService.java
@@ -1,45 +1,70 @@
 package tarefa3.ac2.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import tarefa3.ac2.dtos.CategoriaCursoDTO;
+import tarefa3.ac2.exceptions.GeneralException;
 import tarefa3.ac2.models.CategoriaCurso;
+import tarefa3.ac2.repositories.CategoriaCursoRepository;
 import tarefa3.ac2.services.contracts.CategoriaCursoServiceContract;
 
 @Service
 public class CategoriaCursoService implements CategoriaCursoServiceContract {
 
+    private CategoriaCursoRepository categoriaCursoRepository;
+
+    public CategoriaCursoService(CategoriaCursoRepository categoriaCursoRepository){
+        this.categoriaCursoRepository = categoriaCursoRepository;
+    }
+
     @Override
     public List<CategoriaCursoDTO> obterTodos() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'obterTodos'");
+        List<CategoriaCursoDTO> categoriaList = categoriaCursoRepository.findAll().stream().map(
+            (CategoriaCurso categoria) -> {
+                return CategoriaCursoDTO.builder()
+                    .id(categoria.getId())
+                    .nome(categoria.getNome())
+                    .build();
+            }
+        ).collect(Collectors.toList());
+        return categoriaList;
     }
 
     @Override
     public CategoriaCurso inserir(CategoriaCursoDTO categoriaCursoDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+        CategoriaCurso categoria = new CategoriaCurso();
+        categoria.setId(null);
+        categoria.setNome(categoriaCursoDTO.getNome());
+        categoria.setCursos(null);
+        return categoriaCursoRepository.save(categoria);
     }
 
     @Override
     public CategoriaCursoDTO obterPorId(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'obterPorId'");
+        return categoriaCursoRepository.findById(id).map(
+            (CategoriaCurso categoria) -> {
+                return CategoriaCursoDTO.builder()
+                    .id(categoria.getId())
+                    .nome(categoria.getNome())
+                    .build();       
+            }
+        ).orElseThrow(() -> new GeneralException("Id da categoria não encontrada dentro da base!."));
     }
 
     @Override
     public CategoriaCurso editar(Long id, CategoriaCursoDTO categoriaCursoDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'editar'");
+        CategoriaCursoDTO oldCategoria = this.obterPorId(id);
+        CategoriaCurso categoriaCurso = new CategoriaCurso();
+        categoriaCurso.setId(oldCategoria.getId());
+        categoriaCurso.setNome(oldCategoria.getNome());
+        return categoriaCursoRepository.save(categoriaCurso);
     }
 
     @Override
     public void excluir(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'excluir'");
+        this.categoriaCursoRepository.deleteById(id);
     }
-
-    
 }
